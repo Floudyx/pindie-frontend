2d216edf77a7af6bd7c61c5562170f8a234adc40
Floudyx <ivaan.yudin22@gmail.com>
2024-05-27 01:09:50 +0700

    new

--- app/pixel-games/page.js
+++ app/pixel-games/page.js
@@ -1,6 +1,6 @@
 'use client';
 import { useGetDataByCategory } from "../api/api-hooks";
-import { Preloader } from "../components/preloader/Preloader.jsx";
+import { Preloader } from "../components/Preloader/Preloader.jsx";
 import { CardsList } from "../components/CardsList/CardsList";
 import { endpoints } from "../api/config";
 
