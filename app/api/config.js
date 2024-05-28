2c517ac5f844be52175fe3f704f336abc9b34d78
Floudyx <ivaan.yudin22@gmail.com>
2024-05-28 15:46:04 +0700

    new

--- app/api/config.js
+++ app/api/config.js
@@ -1,8 +1,8 @@
 
-export const BASE_URL = "https://api-code-2.practicum-team.ru";
+export const BASE_URL = "https://pindie-frontend22.nomoredomainswork.ru";
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
-  auth: `${BASE_URL}/auth/local`,
-  me: `${BASE_URL}/users/me`,
+  auth: `${BASE_URL}/auth/login`,
+  me: `${BASE_URL}/me`,
 };
