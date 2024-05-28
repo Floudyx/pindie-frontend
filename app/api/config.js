cedfdb57adc723be47c4e2388571e427fe92a0d8
Floudyx <ivaan.yudin22@gmail.com>
2024-05-28 16:13:41 +0700

    new

--- app/api/config.js
+++ app/api/config.js
@@ -1,5 +1,5 @@
 
-export const BASE_URL = "https://pindie-backed22.nomoredomainswork.ru";
+export const BASE_URL = "https://pindie-backend22.nomoredomainswork.ru";
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
